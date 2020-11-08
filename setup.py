6b3a0eb64dea96c709556b627a2adb806e43ba04
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2020-11-09 00:53:19 +0400

    Prerelease 2
     - Added `Wavelet` class
     - Faster wavelet recomputation
     - Deprecated `wfiltfn`
     - Added `setup.py` and requirements

---
+++ setup.py
@@ -0,0 +1,59 @@
+import os
+import re
+from setuptools import setup, find_packages
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(*parts):
+    with open(os.path.join(current_path, *parts), encoding='utf-8') as reader:
+        return reader.read()
+
+
+def get_requirements(*parts):
+    with open(os.path.join(current_path, *parts), encoding='utf-8') as reader:
+        return list(map(lambda x: x.strip(), reader.readlines()))
+
+
+def find_version(*file_paths):
+    version_file = read_file(*file_paths)
+    version_matched = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                version_file, re.M)
+    if version_matched:
+        return version_matched.group(1)
+    raise RuntimeError('Unable to find version')
+
+
+setup(
+    name="ssqueezepy",
+    version=find_version('ssqueezepy', '__init__.py'),
+    packages=find_packages(exclude=['tests', 'examples']),
+    url="https://github.com/OverLordGoldDragon/ssqueezepy",
+    license="TBD",
+    author="OverLordGoldDragon",
+    author_email="16495490+OverLordGoldDragon@users.noreply.github.com",
+    description=("Synchrosqueezing and wavelet transforms in Python"),
+    long_description=read_file('README.md'),
+    long_description_content_type="text/markdown",
+    keywords=(
+        "signal-processing python synchrosqueezing wavelet-transform"
+    ),
+    install_requires=get_requirements('requirements.txt'),
+    tests_require=["pytest>=4.0", "pytest-cov"],
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
--- ssqueezepy/wavelets.py
+++ ssqueezepy/wavelets.py
@@ -1,9 +1,78 @@
 import numpy as np
-from numba import jit
-from .utils import pi
+from numba import njit
+from types import FunctionType
+
+pi = np.pi
 
 
-@jit(nopython=True)
+class Wavelet():
+    """Wavelet transform function of the wavelet filter in question,
+    Fourier domain.
+
+    _______________________________________________________________________
+    Filter types      Use for synsq?    Parameters (default)
+
+    cmhat             yes               s (1), mu (1)
+    morlet            yes               mu (2*pi)
+    hhhat             yes               mu (5)
+    bump              yes               s (1), mu (5)
+    _______________________________________________________________________
+
+    # Example:
+        psihfn = Wavelet(('bump', {'s': .5, 'mu': 1}), N=1024)
+        plt.plot(psihfn(scale=8))
+    """
+    SUPPORTED = ('morlet', 'bump', 'cmhat', 'hhhat')
+    def __init__(self, wavelet='morlet', N=128):
+        self._validate_and_set_wavelet(wavelet)
+
+        self.xi = _xi(scale=1, N=N)
+
+    def __call__(self, ipt, N=None):
+        """psihfn(ipt) if ipt is np.ndarray, else ipt = scale, and computes
+           psihfn(scale * xi), where `xi` is recomputed if `N` is not None.
+        """
+        if isinstance(ipt, np.ndarray) and ipt.size > 1:
+            return self.fn(ipt)
+        elif N is None:
+            return self.fn(ipt * self.xi)
+        return self.fn(_xi(ipt, N))
+
+    def _validate_and_set_wavelet(self, wavelet):
+        if isinstance(wavelet, FunctionType):
+            self.fn = wavelet
+            self.name = wavelet.__qualname__
+            return
+
+        errmsg = ("`wavelet` must be one of: (1) string name of supported "
+                  "wavelet; (2) tuple of (1) and dict of wavelet parameters "
+                  "(e.g. {'mu': 5}); (3) custom function taking `scale * xi` "
+                  "as input. (got: %s)" % str(wavelet))
+        if not isinstance(wavelet, (tuple, str)):
+            raise TypeError(errmsg)
+        elif isinstance(wavelet, tuple):
+            if not (len(wavelet) == 2 and isinstance(wavelet[1], dict)):
+                raise TypeError(errmsg)
+            wavelet, wavopts = wavelet
+        elif isinstance(wavelet, str):
+            wavopts = {}
+
+        if wavelet not in Wavelet.SUPPORTED:
+            raise ValueError(f"wavelet '{wavelet}' is not supported; pass "
+                             "in fn=custom_fn, or use one of:", ', '.join(
+                                 Wavelet.SUPPORTED))
+        if wavelet == 'morlet':
+            self.fn = morlet(**wavopts)
+        elif wavelet == 'bump':
+            self.fn = bump(**wavopts)
+        elif wavelet == 'cmhat':
+            self.fn = cmhat(**wavopts)
+        elif wavelet == 'hhhat':
+            self.fn = hhhat(**wavopts)
+        self.name = wavelet
+
+
+@njit
 def _xi(scale, N):
     # N=128: [0, 1, 2, ..., 64, -63, -62, ..., -1] * (2*pi / N) * scale
     # N=129: [0, 1, 2, ..., 64, -64, -63, ..., -1] * (2*pi / N) * scale
@@ -15,32 +84,45 @@ def _xi(scale, N):
         xi[i] = (i - N) * h
     return xi
 
-
+#### Wavelet functions ######################################################
 def morlet(mu=5.):
     cs = (1 + np.exp(-mu**2) - 2 * np.exp(-3/4 * mu**2)) ** (-.5)
     ks = np.exp(-.5 * mu**2)
     return lambda w: _morlet(w, mu, cs, ks)
 
 
-@jit(nopython=True)
+@njit
 def _morlet(w, mu, cs, ks):
     return cs * pi**(-1/4) * (np.exp(-.5 * (mu - w)**2)
                               - ks * np.exp(-.5 * w**2))
 
-class Wavelet():
-    def __init__(self, name='morlet', N=None):
-        self.name = name
 
-        if N is not None:
-            self.xi = _xi(1, N)
-        else:
-            self.xi = None
+def bump(mu=5., s=1., om=0.):
+    return lambda w: _bump(w, (w - mu) / s, om, s)
 
-    def __call__(self, scale, N=None):
-        if N is None:
-            if self.N is None:
-                raise ValueError("`N` can't be None if `self.N` is also None.")
-            xi_scale = scale * self.xi
-        else:
-            xi_scale = _xi(scale, N)
-        self._fn(xi_scale)
+
+@njit
+def _bump(w, _w, om, s):
+    return np.exp(2 * pi * 1j * om * w) / s * (
+        np.abs(_w) < .999) * np.exp(-1. / (1 - (_w * (np.abs(_w) < .999))**2)
+                                   ) / .443993816053287
+
+
+def cmhat(mu=1., s=1.):
+    return lambda w: _cmhat(w - mu, s)
+
+
+@njit
+def _cmhat(_w, s):
+    return 2 * np.sqrt(2/3) * pi**(-1/4) * (
+        s**(5/2) * _w**2 * np.exp(-s**2 * _w**2 / 2) * (_w >= 0))
+
+
+def hhhat(mu=5.):
+    return lambda w: _hhhat(w - mu)
+
+
+@njit
+def _hhhat(_w):
+    return 2 / np.sqrt(5) * pi**(-1/4) * (_w * (1 + _w) * np.exp(-1/2 * _w**2)
+                                          ) * (1 + np.sign(_w))
