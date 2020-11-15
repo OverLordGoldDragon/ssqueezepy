6e5ccab6df35ad15dd3e06f502d2c1f026853494
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2020-11-15 12:58:53 +0400

    Docstrings for _cwt.py

--- ssqueezepy/__init__.py
+++ ssqueezepy/__init__.py
@@ -1,6 +1,7 @@
 from . import ssqueezing
 from . import _ssq_cwt
 from . import _cwt
+from . import wavelets
 from . import utils
 from . import synsq_stft
 from . import stft_transforms
@@ -9,9 +10,14 @@ from . import experimental
 from .ssqueezing import *
 from ._ssq_cwt import *
 from ._cwt import *
+from .wavelets import *
 from .utils import *
 from .synsq_stft import *
 from .stft_transforms import *
+
+
+def wavs():
+    return wavelets.Wavelet.SUPPORTED
 
 
 __version__ = '0.5.0rc2'
--- ssqueezepy/_cwt.py
+++ ssqueezepy/_cwt.py
@@ -9,42 +9,75 @@ from .wavelets import Wavelet
 def cwt(x, wavelet, scales='log', dt=1, nv=None, l1_norm=True,
         padtype='symmetric', rpadded=False):
     """Forward continuous wavelet transform, discretized, as described in
-    Sec. 4.3.3 of [1] and Sec. IIIA for [2]. This algorithm uses the FFT and
-    samples the wavelet atoms in the Fourier domain. Options such as padding
-    of the original signal are allowed. Returns the vector of scales and, if
-    requested, the analytic time-derivative of the wavelet transform (as
-    described in Sec. IIIB of [2]).
+    Sec. 4.3.3 of [1] and Sec. IIIA of [2]. Uses a form of discretized
+    convolution theorem via wavelets in the Fourier domain and FFT of input.
+    Returns the vector of scales and the analytic time-derivative of the
+    wavelet transform (as described in Sec. IIIB of [2]).
 
     # Arguments:
-        x: np.ndarray. Input signal vector, length `n` (need not be dyadic).
-        wavelet_type: str. See `wfiltfn`.
-        scales: CWT scales. np.ndarray or ('log', 'linear')
-                !!! beware of scales='linear'; bad current default scheme for
-                capturing low frequencies for sequences longer than 2048.
+        x: np.ndarray. Input signal vector
+        wavelet: str / tuple[str, dict] / `wavelets.Wavelet`.
+            Wavelet sampled in Fourier frequency domain.
+            - str: name of builtin wavelet. `ssqueezepy.wavs()`
+            - tuple[str, dict]: name of builtin wavelet and its configs; e.g.
+              `('morlet', {'mu': 5})`.
+            - `wavelets.Wavelet` instance of. Can use for custom wavelet.
+        scales: str['log', 'linear'] / np.ndarray. CWT scales.
+                - 'log': exponentially distributed scales, as pow of 2:
+                         `[2^(1/nv), 2^(2/nv), ...]`
+                - 'linear': linearly distributed scales. EXPERIMENTAL;
+                  beware of this scheme for input lengths longer than 2048,
+                  as default scheme is poor (and there may not be a good
+                  non-piecewise scheme).
         nv: int. Number of voices. Suggested >= 32.
-        dt: int. sampling period.
-        opts: dict. Options:
-            'padtype': ('symmetric', 'replicate', 'circular'). Type of padding.
-                       (default = 'symmetric')
-            'rpadded': bool. Whether to return padded Wx and dWx.
-                       (default = False)
-            'type', 's', 'mu', ...: str. Wavelet options (see `wfiltfn`).
+        dt: float. sampling period.
+        l1_norm: bool. Whether to L1-normalize the CWT, which yields a more
+           representative distribution of energies and component amplitudes
+           than L2. If False (default True), uses L2 norm.
+        padtype: str. Pad scheme to apply on input.
+            One of: ('zero', 'symmetric', 'replicate', 'circular').
+
+            'zero' is most naive, while 'symmetric' (default) partly mitigates
+            boundary effects. See [3] & [4].
+
+        rpadded: bool. Whether to return padded Wx and dWx (default False,
+             which drops the added padding per `padtype` to return Wx and dWx
+             of .shape[1] == len(x)).
 
     # Returns:
-        Wx: (na x n) size matrix (rows = scales, cols = times), containing
-            samples of the CWT of `x`.
-        scales: `na` length vector containing the associated scales.
-        dWx: (na x n) size matrix containing samples of the time-derivatives
-              of the CWT of `x`.
-        x_mean: mean of `x` to use in inversion (CWT needs scale=inf to capture)
+        Wx: [na x n] np.ndarray (rows=scales, cols=timeshifts); the CWT of `x`.
+            na = number of scales; n = len(x).
+        scales: [na] np.ndarray. Scales at which CWT was computed.
+        dWx: [na x n] np.ndarray. Time-derivatives of the CWT of `x`, computed
+            via frequency-domain differentiation (effectively, derivative of
+            trigonometric interpolation; see [5]).
+        x_mean: float. mean of `x` to use in inversion (CWT needs scale=inf
+            to capture).
 
     # References:
-        1. Mallat, S., Wavelet Tour of Signal Processing 3rd ed.
+        1. Wavelet Tour of Signal Processing, 3rd ed. S. Mallat.
+        https://www.di.ens.fr/~mallat/papiers/WaveletTourChap1-2-3.pdf
 
-        2. G. Thakur, E. Brevdo, N.-S. Fučkar, and H.-T. Wu,
-        "The Synchrosqueezing algorithm for time-varying spectral analysis:
-        robustness properties and new paleoclimate applications,"
-        Signal Processing, 93:1079-1094, 2013.
+        2. The Synchrosqueezing algorithm for time-varying spectral analysis:
+        robustness properties and new paleoclimate applications. G. Thakur,
+        E. Brevdo, N.-S. Fučkar, and H.-T. Wu.
+        https://arxiv.org/abs/1105.0010
+
+        3. Signal extension modes. PyWavelets contributors.
+        https://pywavelets.readthedocs.io/en/latest/ref/
+        signal-extension-modes.html
+
+        4. Wavelet Bases and Lifting Wavelets. H. Xiong.
+        http://min.sjtu.edu.cn/files/wavelet/
+        6-lifting%20wavelet%20and%20filterbank.pdf
+
+        5. The Exponential Accuracy of Fourier and Chebyshev Differencing Methods.
+        E. Tadmor.
+        http://webhome.auburn.edu/~jzl0097/teaching/math_8970/Tadmor_86.pdf
+
+        6. Rectification of the Bias in the Wavelet Power Spectrum.
+        Y. Liu, X. S. Liang, R. H. Weisberg.
+        http://ocg6.marine.usf.edu/~liu/Papers/Liu_etal_2007_JAOT_wavelet.pdf
     """
     def _process_args(x, scales, dt):
         if not dt > 0:
@@ -101,30 +134,55 @@ def cwt(x, wavelet, scales='log', dt=1, nv=None, l1_norm=True,
 
 def icwt(Wx, wavelet, scales='log', one_int=True, x_len=None, x_mean=0,
          padtype='symmetric', rpadded=False, l1_norm=True):
-    """The inverse continuous wavelet transform of signal Wx via double integral.
-    Implements Eq. (4.67) of [1].
+    """The inverse continuous wavelet transform of Wx, via double or
+    single integral.
 
     Inputs:
-       Wx: wavelet transform of a signal, see help cwt_fw
-       type: wavelet used to take the wavelet transform,
-             see help cwt_fw and help wfiltfn
-       opt: options structure used for forward wavelet transform.
-       len_x: length of original x, before padding
-       x_mean: mean of original x (not picked up in CWT since it's an
-                                  infinite scale component)
+        Wx: CWT computed via `ssqueezepy.cwt`.
+        wavelet: str / tuple[str, dict] / `wavelets.Wavelet`.
+            Wavelet sampled in Fourier frequency domain.
+            - str: name of builtin wavelet. `ssqueezepy.wavs()`
+            - tuple[str, dict]: name of builtin wavelet and its configs; e.g.
+              `('morlet', {'mu': 5})`.
+            - `wavelets.Wavelet` instance of. Can use for custom wavelet.
+        one_int: bool. Whether to use one-integral iCWT (default) or double.
+            Current one-integral implementation performs best.
+            - True: Eq 2.6, modified, of [3]. Explained in [4].
+            - False: Eq 4.67 of [1]. Explained in [5].
+        x_len: int / None. Length of `x` used in forward CWT, if different
+            from Wx.shape[1] (default if None).
+        x_mean: float. mean of original `x` (not picked up in CWT since it's an
+            infinite scale component). Default 0.
+        padtype: str. Pad scheme to apply on Wx along timeshift axis (dim 1).
+            One of: ('zero', 'symmetric', 'replicate', 'circular').
+            !!! currently uses only 'zero'
+
+            'zero' is most naive, while 'symmetric' (default) partly mitigates
+            boundary effects. See [3] & [4].
+        rpadded: bool. True if Wx is padded (e.g. if used `cwt(, rpadded=True)`).
+        l1_norm: bool. True if Wx was obtained via `cwt(, l1_norm=True)`.
 
     Output:
-     x: the signal, as reconstructed from Wx
-
-    Explained in: https://dsp.stackexchange.com/a/71148/50076
+     x: np.ndarray. The signal, as reconstructed from Wx.
 
     References:
-        1. Mallat, S., Wavelet Tour of Signal Processing 3rd ed.
+        1. Wavelet Tour of Signal Processing, 3rd ed. S. Mallat.
+        https://www.di.ens.fr/~mallat/papiers/WaveletTourChap1-2-3.pdf
 
-        2. G. Thakur, E. Brevdo, N.-S. Fuckar, and H.-T. Wu,
-        "The Synchrosqueezing algorithm for time-varying spectral analysis:
-         robustness properties and new paleoclimate applications",
-         Signal Processing, 93:1079-1094, 2013.
+        2. The Synchrosqueezing algorithm for time-varying spectral analysis:
+        robustness properties and new paleoclimate applications. G. Thakur,
+        E. Brevdo, N.-S. Fučkar, and H.-T. Wu.
+        https://arxiv.org/abs/1105.0010
+
+        3. Synchrosqueezed Wavelet Transforms: a Tool for Empirical Mode
+        Decomposition. I. Daubechies, J. Lu, H.T. Wu.
+        https://arxiv.org/pdf/0912.2437.pdf
+
+        4. One integral inverse CWT. OverLordGoldDragon.
+        https://dsp.stackexchange.com/a/71274/50076
+
+        5. Inverse CWT derivation. OverLordGoldDragon.
+        https://dsp.stackexchange.com/a/71148/50076
     """
     #### Prepare for inversion ###############################################
     na, n = Wx.shape
@@ -161,9 +219,7 @@ def icwt(Wx, wavelet, scales='log', one_int=True, x_len=None, x_mean=0,
 
 
 def _icwt_2int(Wx, scales, wavelet, N, freqscale, l1_norm):
-    """Double-integral iCWT; works with any(?) wavelet.
-    Explanation: https://dsp.stackexchange.com/a/71148/50076
-    """
+    """Double-integral iCWT; works with any(?) wavelet."""
     norm = _icwt_norm(freqscale, l1_norm, one_int=False)
     x = np.zeros(N)
     for a, Wxa in zip(scales, Wx):  # TODO vectorize?
@@ -174,9 +230,7 @@ def _icwt_2int(Wx, scales, wavelet, N, freqscale, l1_norm):
 
 
 def _icwt_1int(Wx, scales, freqscale, l1_norm):
-    """One-integral iCWT; assumes analytic wavelet.
-    Explanation: https://dsp.stackexchange.com/a/71274/50076
-    """
+    """One-integral iCWT; assumes analytic wavelet."""
     norm = _icwt_norm(freqscale, l1_norm, one_int=True)
     return (Wx.real / (norm(scales))).sum(axis=0)
 
