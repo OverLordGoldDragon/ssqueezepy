06c85bce94fa879ca00952b21efc0d0235c537ba
David Bondesson <d919.bondesson@gmail.com>
2021-01-21 11:29:45 +0100

    updated ridge_tracking test with appropriate signal padding and test images

--- tests/ridge_tracking_test.py
+++ tests/ridge_tracking_test.py
@@ -13,16 +13,23 @@ from ssqueezepy import ridge_tracking as rt
 
 def viz(signal, tf_transf,ridge,flip_plot=False):
     plt.plot(signal)
+    plt.title('Time signal')
+    plt.xlabel('Time axis [A.U].')
+    plt.ylabel('Signal Amplitude [A.U.]')
     plt.show()
     
     plt.figure()
     if(flip_plot):
         plt.imshow(np.flipud(np.abs(tf_transf)), aspect='auto', vmin=0, vmax=np.max(np.abs(tf_transf)), cmap='jet')
         plt.plot(len(Txo)-ridge,linestyle = '--',color='black')
+        plt.title('Syncrosqueezed continouse wavelet transform w. ridge')
     else:
         plt.imshow(np.abs(tf_transf), aspect='auto', cmap='jet')
         plt.plot(ridge,linestyle = '--',color='black')
-    
+        plt.title('Continouse wavelet transform w. ridge')
+   
+    plt.xlabel('Time axis [A.U].')
+    plt.ylabel('Frequency scales [A.U.]')
     plt.show()
     
     
@@ -36,7 +43,7 @@ print('ridge follows indexes:', ridge_idx)
 ### Example 1: Two constant frequency signals
     
 sig_lgth = 500
-t_vec=np.linspace(0,10,sig_lgth)
+t_vec=np.linspace(0,10,sig_lgth,endpoint=True)
 
 f1=0.5
 f2=2.0
@@ -44,19 +51,20 @@ signal_1=np.sin(f1*2*np.pi*t_vec)
 signal_2=np.cos(f2*2*np.pi*t_vec)
 
 signal=signal_1+signal_2
+padtype = 'wrap'
 
-Txo, ssq_freq, Wxo, scales_xo, _ = ssq_cwt(signal,t=t_vec)
+Txo, ssq_freq, Wxo, scales_xo, _ = ssq_cwt(signal,t=t_vec,padtype = padtype)
 Wxo /= np.sqrt(scales_xo)  # L1 norm
 
 
-penalty=2.0
+penalty=20.0
 # CWT example
 Energy,ridge_idx,_ = rt.extract_fridges(Wxo,scales_xo,penalty,num_ridges=2,BW=25)
 plt.figure()
 viz(signal, Wxo, ridge_idx)
 
 # syncrosqueezed example
-max_Energy,ridge_idx,_ = rt.extract_fridges(Txo,scales_xo,penalty,num_ridges=2,BW=2)
+max_Energy,ridge_idx,_ = rt.extract_fridges(Txo,scales_xo,penalty,num_ridges=2,BW=4)
 plt.figure()
 viz(signal, Txo, ridge_idx,flip_plot=True)
 
@@ -68,11 +76,11 @@ sign_chirp_2 = chirp(t_vec, f0=.4, f1=4, t1=20, method='quadratic')
 
 sign_chirp=sign_chirp_1+sign_chirp_2
 
-
-Txo, ssq_freq, Wxo, scales_xo, _ = ssq_cwt(sign_chirp, wavelet=('morlet'),t=t_vec)
+padtype = 'reflect' # standard padding for ssq_cwt
+Txo, ssq_freq, Wxo, scales_xo, _ = ssq_cwt(sign_chirp,t=t_vec,padtype = padtype)
 Wxo /= np.sqrt(scales_xo)  # L1 norm
 
-penalty=.3
+penalty=0.5
 
 # CWT example
 Energy,ridge_idx,_ = rt.extract_fridges(Wxo,scales_xo,penalty,num_ridges=2,BW=25)
@@ -96,8 +104,9 @@ sweep_sig_1 = sweep_poly(t_vec, p1)+signal_1
 
 
 sweep_sig=sweep_sig_1
+padtype = 'wrap'
 
-Txo, ssq_freq, Wxo, scales_xo, _ = ssq_cwt(sweep_sig, wavelet=('morlet'),t=t_vec)
+Txo, ssq_freq, Wxo, scales_xo, _ = ssq_cwt(sweep_sig,t=t_vec,padtype = padtype)
 Wxo /= np.sqrt(scales_xo)  # L1 norm
 
 penalty=2.0
