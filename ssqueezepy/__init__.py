65ab04bdfaf355c2b24470423aabf2a3f6f2b703
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2021-08-22 07:54:27 +0400

    Update __init__.py

--- ssqueezepy/__init__.py
+++ ssqueezepy/__init__.py
@@ -34,6 +34,16 @@ __license__ = __doc__
 __project_url__ = 'https://github.com/OverLordGoldDragon/ssqueezepy'
 
 
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    class PltDummy():
+        def __getattr__(self, name):
+            raise ValueError("`ssqueezepy.visuals` requires "
+                             "`matplotlib` installed.")
+    plt = PltDummy()
+
+
 from . import utils
 from . import ssqueezing
 from . import _cwt
@@ -68,15 +78,6 @@ from .configs import IS_PARALLEL, USE_GPU
 def wavs():
     return wavelets.Wavelet.SUPPORTED
 
-
-try:
-    import matplotlib.pyplot as plt
-except ImportError:
-    class PltDummy():
-        def __getattr__(self, name):
-            raise ValueError("`ssqueezepy.visuals` requires "
-                             "`matplotlib` installed.")
-    plt = PltDummy()
 
 _modules_toplevel = [
     '_cwt', '_gmw', '_ssq_cwt', '_ssq_stft', '_stft', '_test_signals', 'algos',
